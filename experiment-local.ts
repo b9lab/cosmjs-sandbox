0106c36abb6cce23161dd949ba28c20552c3200c
Xavier Leprêtre <xavier.lepretre@gmail.com>
2022-05-06 22:51:46 +0400

    Use local Simd to send tokens.

---
+++ experiment-local.ts
@@ -0,0 +1,46 @@
+import { readFile } from "fs/promises"
+import { fromHex } from "@cosmjs/encoding"
+import { DirectSecp256k1Wallet, OfflineDirectSigner } from "@cosmjs/proto-signing"
+import { SigningStargateClient, StargateClient } from "@cosmjs/stargate"
+
+const rpc = "http://127.0.0.1:26657"
+
+const getAliceSignerFromPriKey = async (): Promise<OfflineDirectSigner> => {
+    return DirectSecp256k1Wallet.fromKey(
+        fromHex((await readFile("./simd.alice.private.key")).toString()),
+        "cosmos"
+    )
+}
+
+const runAll = async (): Promise<void> => {
+    const client = await StargateClient.connect(rpc)
+    console.log("With client, chain id:", await client.getChainId(), ", height:", await client.getHeight())
+    console.log(
+        "Alice balances:",
+        await client.getAllBalances("cosmos17tvd4hcszq7lcxuwzrqkepuau9fye3dal606zf")
+    )
+    const faucet: string = "cosmos1umpxwaezmad426nt7dx3xzv5u0u7wjc0kj7ple"
+
+    const aliceSigner: OfflineDirectSigner = await getAliceSignerFromPriKey()
+    const alice = (await aliceSigner.getAccounts())[0].address
+    console.log("Alice's address from signer", alice)
+    const signingClient = await SigningStargateClient.connectWithSigner(rpc, aliceSigner)
+    console.log(
+        "With signing client, chain id:",
+        await signingClient.getChainId(),
+        ", height:",
+        await signingClient.getHeight()
+    )
+
+    console.log("Alice balance before:", await client.getAllBalances(alice))
+    console.log("Faucet balance before:", await client.getAllBalances(faucet))
+    const result = await signingClient.sendTokens(alice, faucet, [{ denom: "stake", amount: "100000" }], {
+        amount: [{ denom: "stake", amount: "500" }],
+        gas: "200000",
+    })
+    console.log("Transfer result:", result)
+    console.log("Alice balance after:", await client.getAllBalances(alice))
+    console.log("Faucet balance after:", await client.getAllBalances(faucet))
+}
+
+runAll()
